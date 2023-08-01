22c95c2a38af8fc207763c9d8b52fcb5ce03b811
Vlad Melnik <vmelniki2828@gmail.com>
2023-08-02 00:26:55 +0300

    FIX

--- src/Api/API.js
+++ src/Api/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://649c1e0a0480757192377b6a.mockapi.io/'
 
 export const getContacts = async () => {
-    const { data } = await axios.get('/Phonebook');
+    const { data } = await axios.get('/contacts');
     return data;
 }
 
